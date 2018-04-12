f8479ad2bd3da2ed25e655b4b13f50fdeccd994a
Adam Irby <adam.irby@gmail.com>
2018-04-11 18:29:10 -0700

    Added dice class

---
+++ lab15_Irby.py
@@ -0,0 +1,29 @@
+#Adam Irby
+#CST 205 Lab 15
+#=-=-=-=-=-=-=-=
+import random
+
+
+class dice:
+  numOfDice = None
+  sidesOfDie = None
+  rolls = []
+  
+  def __init__(self, numOfDice, sidesOfDie):
+    self.sidesOfDie = sidesOfDie
+    self.numOfDice = numOfDice
+    
+    for i in range(0, numOfDice):
+      self.rolls.append(random.randrange(self.sidesOfDie))
+      
+  
+  def rollAllDice(self):
+    for i in range(0, self.numOfDice):
+      self.rolls[i] = random.randrange(self.sidesOfDie)
+      
+  def rollSingleDie(self, dieToRoll):
+    if dieToRoll < len(self.rolls):
+      self.rolls[dieToRoll] = random.randrange(self.sidesOfDie)
+      
+  def getRolls(self):
+    return self.rolls
